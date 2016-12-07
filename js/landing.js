627d0c3675c3d6dbfaa3117d83c98c7f61e05603
Ross Waguespack <rosswaguespack@gmail.com>
2016-12-06 19:51:30 -0600

    Fix Type Error

---
+++ js/landing.js
@@ -0,0 +1,32 @@
+var pointsArray = document.getElementsByClassName('point');
+
+ var animatePoints = function(points){
+
+  function revealPoints(elems){
+    for (var i = 0; i < elems.length; i++){
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+  }
+
+  revealPoints(points);
+
+};
+
+window.onload = function() {
+
+  if(window.innerHeight >= 950){
+    animatePoints(pointsArray);
+  };
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener("scroll", function(event) {
+   if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+     animatePoints(pointsArray);
+    }
+  });
+ }
